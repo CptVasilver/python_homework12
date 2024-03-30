c514ec34ede230ee9b05184ba8b0cf405abd17da
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-03-30 20:09:46 +0300

    try

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,34 +7,44 @@ from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 import os
 
+DEFAULT_BROWSER_VERSION = "122.0"
+
 
 def path(file_name):
     return str(Path(__file__).parent.joinpath(f'resources/{file_name}'))
 
 
-@pytest.fixture(scope="session", autouse=True)
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='122.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
 
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def browser_management(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "122.0",
+        "browserVersion": "browser_version",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
